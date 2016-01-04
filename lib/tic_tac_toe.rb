45aa00a09581402e410f01ead5f6df2afc9d92ae
James McCloskey <james@mccloskey.io>
2016-01-03 19:30:48 -0600

    implement Array.all_empty? method

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -3,6 +3,7 @@ require 'tic_tac_toe/version'
 require 'tic_tac_toe/cell'
 require 'tic_tac_toe/player'
 require 'tic_tac_toe/board'
+require 'tic_tac_toe/core_extensions'
 
 module TicTacToe
   # Your code goes here...
