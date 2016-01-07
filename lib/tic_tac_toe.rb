fec1ed18c87f137fac923048496f81aef8f13b87
James McCloskey <j.mccloskey89@gmail.com>
2016-01-07 07:07:13 +0000

    update require statements and add an example game script

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,9 +1,10 @@
 # lib/tic_tac_toe.rb
-require 'tic_tac_toe/version'
-require 'tic_tac_toe/cell'
-require 'tic_tac_toe/player'
-require 'tic_tac_toe/board'
-require 'tic_tac_toe/core_extensions'
+require_relative 'tic_tac_toe/version'
+require_relative 'tic_tac_toe/cell'
+require_relative 'tic_tac_toe/player'
+require_relative 'tic_tac_toe/board'
+require_relative 'tic_tac_toe/core_extensions'
+require_relative 'tic_tac_toe/game'
 
 module TicTacToe
   # Your code goes here...
